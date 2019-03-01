888fbf6c52f5bf463b9ec15e235ffc2b1ec12d0c
Rahat <37264964+rahxt@users.noreply.github.com>
2019-03-01 12:42:39 +0600

    aqi colour

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 var z = new XMLHttpRequest();
 function info() {
-    document.getElementById("data1").innerHTML = "locating . . .";
-    document.getElementById("data3").innerHTML = " ";
-	z.open("GET", "//api.airvisual.com/v2/nearest_city?key=pWJrvDbBDbhoQjqhj", true);
+    document.getElementById("l1").innerHTML = "locating . . .";
+	z.open("GET", "http://api.airvisual.com/v2/nearest_city?key=pWJrvDbBDbhoQjqhj", true);
 	z.send();
 z.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var leObj = JSON.parse(this.responseText);
-    document.getElementById("data1").innerHTML = leObj.data.city +", "+ leObj.data.country+". ";
-    document.getElementById("data3").innerHTML = leObj.data.current.pollution.aqius;
+    document.getElementById("l1").innerHTML = leObj.data.city +", "+ leObj.data.country+". ";
+    //document.getElementById("data3").innerHTML = leObj.data.current.pollution.aqius;
+    k = leObj.data.current.pollution.aqius;
+
+  if(k < 50) {
+      document.getElementById("a1").innerHTML = k;
+  } else if (k < 100) {
+      document.getElementById("a2").innerHTML = k;
+  } else if (k < 150) {
+      document.getElementById("a3").innerHTML = k;
+  } else if (k < 200) {
+      document.getElementById("a4").innerHTML = k;
+  } else if (k < 300) {
+      document.getElementById("a5").innerHTML = k;
+  } else {
+      document.getElementById("a6").innerHTML = k;
+  }
+
+  if(k < 50) {
+      document.getElementById("b1").innerHTML = "Good";
+  } else if (k < 100) {
+      document.getElementById("b2").innerHTML = "Moderate";
+  } else if (k < 150) {
+      document.getElementById("b3").innerHTML = "Unhealthy for sensitive groups";
+  } else if (k < 200) {
+      document.getElementById("b4").innerHTML = "Unhealthy";
+  } else if (k < 300) {
+      document.getElementById("b5").innerHTML = "Very Unhealthy";
+  } else {
+      document.getElementById("b6").innerHTML = "Hazardous";
+  }
+      // code block
+
+  //  document.getElementById("data3").innerHTML = k;
+
   }
 };
 }
-
-/*
-var x = document.getElementById("buttonid");
-  function gpsX() {
-	if (navigator.geolocation) {
-   	  navigator.geolocation.getCurrentPosition(locationX, errorX);
-  	  } else {
-   	  x.innerHTML = "UNABLE TO DETECT LOCATION";
-  	  }
-	}
-  function locationX(position) {
-  	x.innerHTML = "LOCATION: " + position.coords.latitude + " . " + position.coords.longitude;
-  }
-  function errorX(error) {
- 	switch(error.code) {
-   	  case error.POSITION_UNAVAILABLE:
-      x.innerHTML = "ERROR"
-   	  break;
-    }
-  }
-*/
