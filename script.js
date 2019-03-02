0cfff9eb97a878b11a4b331bb218ed8668d37021
Rahat <37264964+rahxt@users.noreply.github.com>
2019-03-02 15:35:13 +0600

    progress bar added

--- script.js
+++ script.js
@@ -11,10 +11,9 @@ function info() {
       k = leObj.data.current.pollution.aqius;
       city = leObj.data.city
       country = leObj.data.country
-
+      p = (k * 100) / 600
+      m = 100 - p - 10
       document.getElementById("l1").innerHTML = city +", "+ country +". ";
-
-
 
   if(k < 50) {
       document.getElementById("a1").innerHTML = k;
@@ -45,4 +44,37 @@ function info() {
   }
   }
   };
+
+    document.getElementById("progress").style.display="block";
+    var elem = document.getElementById("bar");
+    var width = 0;
+    var id = setInterval(frame, 0);
+    function frame() {
+      if (width >= m) {
+        clearInterval(id);
+      } else {
+        width++;
+        elem.style.width = width + '%';
+
+        if(m < 40) {
+          document.getElementById("bar").style.backgroundColor="#b700b7";
+          elem.innerHTML = "Hazardous";
+        } else if (m < 57) {
+          elem.innerHTML = "Very Unhealthy";
+          document.getElementById("bar").style.backgroundColor="#ff0000";
+        } else if (m < 74) {
+          elem.innerHTML = "Unhealthy";
+          document.getElementById("bar").style.backgroundColor="#ff8000";
+        } else if (m < 82) {
+          document.getElementById("bar").style.backgroundColor="#dddd00";
+          elem.innerHTML = "Modetarate";
+        } else if (m < 100) {
+          document.getElementById("bar").style.backgroundColor="#00a800";
+          elem.innerHTML = "Good";
+        } else {
+          document.getElementById("bar").style.backgroundColor="#00a800";
+          elem.innerHTML = "Good";
+        }
+      }
+    }
 }
