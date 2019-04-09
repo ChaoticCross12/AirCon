9f382dd2c6339f74e6e462d5529bb1948a686b8d
Rahat <37264964+rahxt@users.noreply.github.com>
2019-04-09 13:45:38 +0600

    onclick//color update

--- script.js
+++ script.js
@@ -14,28 +14,28 @@ function info() {
       var country = leObj.data.country
       p = 100 - ((k * 100) / 600) - 10  //percentage calculation taking 600 as max AQI //-10 for stability
   //show location
-  document.getElementById("l1").innerHTML = city +", "+ country +". ";
+      document.getElementById("l1").innerHTML = city +", "+ country +". ";
   //show AQI
-  if(k < 50) {
-      document.getElementById("a1").innerHTML = k;
-      document.getElementById("b1").innerHTML = "Good";
-  } else if (k < 100) {
-      document.getElementById("a2").innerHTML = k;
-      document.getElementById("b2").innerHTML = "Moderate";
-  } else if (k < 150) {
-      document.getElementById("a3").innerHTML = k;
-      document.getElementById("b3").innerHTML = "Unhealthy for sensitive groups";
-  } else if (k < 200) {
-      document.getElementById("a4").innerHTML = k;
-      document.getElementById("b4").innerHTML = "Unhealthy";
-  } else if (k < 300) {
-      document.getElementById("a5").innerHTML = k;
-      document.getElementById("b5").innerHTML = "Very Unhealthy";
-  } else {
-      document.getElementById("a6").innerHTML = k;
-      document.getElementById("b6").innerHTML = "Hazardous";
-  }
-  }
+      if(k < 50) {
+          document.getElementById("a1").innerHTML = k;
+          document.getElementById("b1").innerHTML = "Good";
+      } else if (k < 100) {
+          document.getElementById("a2").innerHTML = k;
+          document.getElementById("b2").innerHTML = "Moderate";
+      } else if (k < 150) {
+          document.getElementById("a3").innerHTML = k;
+          document.getElementById("b3").innerHTML = "Unhealthy for sensitive groups";
+      } else if (k < 200) {
+          document.getElementById("a4").innerHTML = k;
+          document.getElementById("b4").innerHTML = "Unhealthy";
+      } else if (k < 300) {
+          document.getElementById("a5").innerHTML = k;
+          document.getElementById("b5").innerHTML = "Very Unhealthy";
+      } else {
+          document.getElementById("a6").innerHTML = k;
+          document.getElementById("b6").innerHTML = "Hazardous";
+      }
+      }
   };
     //unhide progress bar
     document.getElementById("progress").style.display="block";
@@ -54,10 +54,10 @@ function info() {
           elem.innerHTML = "Hazardous";
         } else if (p < 57) {
           elem.innerHTML = "Very Unhealthy";
-          document.getElementById("bar").style.backgroundColor="#ff0000";
+          document.getElementById("bar").style.backgroundColor="#b700b7";
         } else if (p < 74) {
           elem.innerHTML = "Unhealthy";
-          document.getElementById("bar").style.backgroundColor="#ff8000";
+          document.getElementById("bar").style.backgroundColor="#ff3600";
         } else if (p < 82) {
           document.getElementById("bar").style.backgroundColor="#dddd00";
           elem.innerHTML = "Modetarate";
